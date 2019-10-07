96f893ea9d12d96dd68336754905d28354af0a0b
k.neyman <k.neyman@corp.mail.ru>
2019-10-07 03:59:59 +0300

    Add multiaction

--- lesson3/src/main/java/superp/techpark/ru/lesson3/fragment_part/activity/MultiActionActivity.java
+++ lesson3/src/main/java/superp/techpark/ru/lesson3/fragment_part/activity/MultiActionActivity.java
@@ -2,8 +2,8 @@ package superp.techpark.ru.lesson3.fragment_part.activity;
 
 import android.os.Bundle;
 
-import java.util.Random;
-
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import superp.techpark.ru.lesson3.BaseActivity;
 import superp.techpark.ru.lesson3.R;
 import superp.techpark.ru.lesson3.fragment_part.fragment.GreenFragment;
@@ -15,7 +15,8 @@ import superp.techpark.ru.lesson3.fragment_part.fragment.RedFragment;
  * операций с фрагментами.
  */
 public class MultiActionActivity extends BaseActivity {
-    private final Random mRandom = new Random();
+
+    private static int sNextNum = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,13 +24,27 @@ public class MultiActionActivity extends BaseActivity {
         setContentView(R.layout.activity_multi_action);
 
         if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.top_container, new RedFragment())
-                    .commit();
-
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.bottom_container, GreenFragment.newInstance(mRandom.nextInt(100)))
-                    .commit();
+            toggleState();
         }
+
+        findViewById(R.id.btn_switch).setOnClickListener(view -> toggleState());
+    }
+
+    private void toggleState() {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        Fragment top = getSupportFragmentManager().findFragmentById(R.id.top_container);
+        Fragment bottom = getSupportFragmentManager().findFragmentById(R.id.bottom_container);
+
+        if (top != null && top.isAdded()) {
+            transaction.remove(top);
+            transaction.add(R.id.bottom_container, GreenFragment.newInstance(sNextNum++));
+        } else if (bottom != null && bottom.isAdded()) {
+            transaction.remove(bottom);
+            transaction.add(R.id.top_container, new RedFragment());
+        } else {
+            transaction.add(R.id.top_container, new RedFragment());
+        }
+
+        transaction.commit();
     }
 }
