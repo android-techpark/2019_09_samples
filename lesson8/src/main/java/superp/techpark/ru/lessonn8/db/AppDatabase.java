d64c4ecd95fa58c4c22a9a61f506e029066962b1
Andrey Govorovskiy <a.govorovskiy@corp.mail.ru>
2019-11-18 03:34:43 +0300

    add lesson8

---
+++ lesson8/src/main/java/superp/techpark/ru/lessonn8/db/AppDatabase.java
@@ -0,0 +1,32 @@
+package superp.techpark.ru.lessonn8.db;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {Person.class , SimpleEntity.class, /* AThirdEntityType.class */}, version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+
+    public abstract PersonDao getPersonDao();
+
+    public abstract SimpleEntityDao getDao();
+
+    private static AppDatabase instance;
+
+    static synchronized AppDatabase getInstance(Context context) {
+        if (instance == null) {
+            instance = create(context);
+        }
+        return instance;
+    }
+
+
+    private static AppDatabase create(final Context context) {
+        return Room.databaseBuilder(
+                context,
+                AppDatabase.class,
+                "my_db_name").build();
+    }
+}
---
+++ lesson8/src/main/java/superp/techpark/ru/lessonn8/db/SimpleEntityDao.java
@@ -0,0 +1,24 @@
+package superp.techpark.ru.lessonn8.db;
+
+import java.util.List;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+@Dao
+interface SimpleEntityDao {
+
+
+    @Insert
+    void insertAll(SimpleEntity... entities);
+
+
+    // Получение всех Person из бд
+    @Query("SELECT * FROM simple_entity")
+    List<SimpleEntity> getAllEntities();
+
+    @Delete
+    void delete(SimpleEntity... entities);
+}
